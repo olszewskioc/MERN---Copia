dd8ee926bab4698b4b6e9ad50dcc5bc8ff58229a
olszewskioc <thiago.olszewskioc@gmail.com>
2024-03-16 13:47:42 -0400

    Project init

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import dotenv from  'dotenv';
+dotenv.config();
+import connectToDatabase from './db.js';
+import express from  'express';
+import cors from 'cors';
+
+// Routes
+import productRoutes from './routes/productRoutes.js';
+
+
+connectToDatabase();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/products', productRoutes);
+// localhost:5000/api/products
+
+const port  = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the API');
+});
+
+app.listen(port, () => {
+    console.log(`Server run in port ${port}`);
+});
